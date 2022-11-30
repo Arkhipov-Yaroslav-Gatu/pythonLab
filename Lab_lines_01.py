c6475c80b644d2e991e2ffc3f1577917aef369e0
yaroslav <arhipovyaroslav@yandex.ru>
2022-12-01 01:31:02 +0500

    python commit

---
+++ Lab_lines_01.py
@@ -0,0 +1,17 @@
+# 1. Палиндром
+
+def is_palindrome(line):
+    print(line)
+    r_line = line[::-1]
+    return line == r_line
+
+line = str(input("Введите строку - "))
+
+line = line.lower()
+symbols = "!?-+* .,':;="
+
+for i in range(len(symbols)):
+    line = line.replace(symbols[i], "")
+    #  print(line)
+
+print(is_palindrome(line))
